7c8f4e93f4459cd7760d123bf2be8c4127773905
David Boclé <david.bocle@finastra.com>
2022-06-09 14:07:33 +0200

    Output mongo URI at boot time

--- apps/server/src/configs/mongoose.config.ts
+++ apps/server/src/configs/mongoose.config.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongooseModuleOptions, MongooseOptionsFactory } from '@nestjs/mongoose';
 
 @Injectable()
 export class MongooseConfigService implements MongooseOptionsFactory {
+  readonly logger = new Logger(MongooseConfigService.name);
+
   constructor(private configService: ConfigService) {}
 
   createMongooseOptions(): MongooseModuleOptions {
+    const uri = this.configService.get<string>('MONGO_URI');
+    this.logger.log(uri);
     return {
-      uri: this.configService.get<string>('MONGO_URI'),
+      uri,
     };
   }
 }
