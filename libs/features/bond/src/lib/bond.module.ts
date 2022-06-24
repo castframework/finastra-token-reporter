07a2325e640922391052b41bf847c00477cc889b
David Boclé <david.bocle@finastra.com>
2022-06-24 17:09:18 +0200

    add tooltip for nominal

--- libs/features/bond/src/lib/bond.module.ts
+++ libs/features/bond/src/lib/bond.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 import { SkeletonTextModule } from '@finastra/angular-components/skeleton-text';
 import { SharedModule } from 'libs/shared/src';
 import { BondComponent } from './bond.component';
-import { TransactionsComponent } from './transactions/transactions.component';
-import { HoldersComponent } from './holders/holders.component';
 import { HistoryComponent } from './history/history.component';
+import { HoldersComponent } from './holders/holders.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 @NgModule({
   imports: [
@@ -20,6 +21,7 @@ import { HistoryComponent } from './history/history.component';
     SharedModule,
     MatTabsModule,
     SkeletonTextModule,
+    MatTooltipModule,
   ],
   declarations: [BondComponent, TransactionsComponent, HoldersComponent, HistoryComponent],
 })
