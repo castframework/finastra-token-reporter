53b17a4c8470f4346d4d91bf0d713154387cee4d
David Boclé <david.bocle@finastra.com>
2022-06-29 13:46:40 +0200

    visual improvements to LEI

--- libs/cdk/lei/src/lib/lei.module.ts
+++ libs/cdk/lei/src/lib/lei.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { LeiComponent } from './lei.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatTooltipModule],
   declarations: [LeiComponent],
   exports: [LeiComponent],
 })
