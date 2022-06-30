340af53d0b6184cc7f1af2bfc7ace3cb387ef0aa
David Boclé <david.bocle@finastra.com>
2022-06-30 20:18:00 +0200

    fix lei length

--- libs/cdk/lei/src/lib/lei.module.ts
+++ libs/cdk/lei/src/lib/lei.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { SharedModule } from '@finastra/shared';
 import { LeiComponent } from './lei.component';
 
 @NgModule({
-  imports: [CommonModule, MatTooltipModule],
+  imports: [CommonModule, MatTooltipModule, SharedModule],
   declarations: [LeiComponent],
   exports: [LeiComponent],
 })
--- libs/shared/src/lib/pipes/eth-short.pipe.ts
+++ libs/shared/src/lib/pipes/eth-short.pipe.ts
@@ -2,8 +2,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'ethShort' })
 export class ETHShortPipe implements PipeTransform {
-  constructor() { }
-  transform(ethAddress: string) {
-    return `${ethAddress.substring(0, 10)}...`;
+  constructor() {}
+
+  transform(ethAddress: string, limit = 10) {
+    return ethAddress.length > limit ? `${ethAddress.substring(0, limit)}...` : ethAddress;
   }
 }
